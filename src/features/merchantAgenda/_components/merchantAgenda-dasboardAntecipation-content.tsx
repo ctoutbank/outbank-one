185025cf143d7452df2bd3fdc41f3d0cf42b1f8e
Hebert Nunes <hebertrodrigues.pt@gmail.com>
2025-06-06 19:46:48 +0100

    corrections

--- src/features/merchantAgenda/_components/merchantAgenda-dasboardAntecipation-content.tsx
+++ src/features/merchantAgenda/_components/merchantAgenda-dasboardAntecipation-content.tsx
@@ -49,6 +49,7 @@ export function MerchantAgendaAnticipationsDashboardContent({
                     year: "numeric",
                     hour: "2-digit",
                     minute: "2-digit",
+                    timeZone: "America/Sao_Paulo",
                   })}
                 </p>
               </CardHeader>
--- src/features/merchantAgenda/_components/merchantAgenta-adjustments-dashboard-content.tsx
+++ src/features/merchantAgenda/_components/merchantAgenta-adjustments-dashboard-content.tsx
@@ -76,6 +76,7 @@ export function MerchantAdjustmentsDashboardContent({
                     year: "numeric",
                     hour: "2-digit",
                     minute: "2-digit",
+                    timeZone: "America/Sao_Paulo",
                   })}
                 </p>
               </CardHeader>
