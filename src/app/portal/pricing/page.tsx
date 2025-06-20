5ffa8ad3c28bd1b1101ac2b4cf21e6aa646c21fe
gabmira14 <gmmscontato@gmail.com>
2025-06-20 14:31:20 -0300

    app layout fixed

--- src/app/portal/pricing/page.tsx
+++ src/app/portal/pricing/page.tsx
@@ -1,29 +1,39 @@
-import BaseBody from "@/components/layout/base-body";
 import BaseHeader from "@/components/layout/base-header";
-import { getFeesAction } from "@/features/newTax/server/fee-db";
-
+import BaseBody from "@/components/layout/base-body";
 import FeeList from "@/features/newTax/_components/new-tax-list";
+import PaginationWithSizeSelector from "@/components/pagination-with-size-selector";
+import { getFeesAction } from "@/features/newTax/server/fee-db"
 
-export const revalidate = 0;
+export default async function NewTaxPage({ searchParams }: { searchParams: { page?: string; pageSize?: string } }) {
+    const page = Number(searchParams.page) || 1;
+    const pageSize = Number(searchParams.pageSize) || 10;
 
-export default async function NewTaxPage() {
-  // Buscar taxas no servidor usando a action
-  const fees = await getFeesAction();
-  console.log("fees", fees);
+    const { fees, totalRecords, currentPage, pageSize: returnedPageSize } = await getFeesAction(page, pageSize);
 
-  return (
-    <>
-      <BaseHeader
-        breadcrumbItems={[
-          { title: "Cadastro de Taxas de Cobrança", url: "/portal/pricing" },
-        ]}
-      />
-      <BaseBody
-        title="Cadastro de Taxas de Cobrança"
-        subtitle={"Taxas de cobrança que serão aplicadas nas transações"}
-      >
-        <FeeList fees={fees} />
-      </BaseBody>
-    </>
-  );
+    return (
+        <>
+            <BaseHeader
+                breadcrumbItems={[
+                    { title: "Cadastro de Taxas de Cobrança", url: "/portal/pricing" },
+                ]}
+            />
+            <BaseBody
+                title="Cadastro de Taxas de Cobrança"
+                subtitle="Taxas de cobrança que serão aplicadas nas transações"
+            >
+                <FeeList fees={fees} />
+
+                {totalRecords > 0 && (
+                    <div className="mt-2">
+                        <PaginationWithSizeSelector
+                            totalRecords={totalRecords}
+                            currentPage={currentPage}
+                            pageSize={returnedPageSize}
+                            pageName="portal/pricing"
+                        />
+                    </div>
+                )}
+            </BaseBody>
+        </>
+    );
 }
