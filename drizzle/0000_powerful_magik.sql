9a4e4037b106aa43625aec775da57048fda65624
Caio Magalhães <cto@outbank.com.br>
2025-10-23 12:59:11 -0300

    awai uncomment

--- drizzle/0000_powerful_magik.sql
+++ drizzle/0000_powerful_magik.sql
@@ -1,6 +1,6 @@
 -- Current sql file was generated after introspecting the database
 -- If you want to run this migration please uncomment this code before executing migrations
-/*
+
 CREATE TABLE IF NOT EXISTS "transactions" (
 	"slug" uuid PRIMARY KEY NOT NULL,
 	"active" boolean,
